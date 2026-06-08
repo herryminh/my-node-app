e9ed9e5e6cdf5fa2fa479cc253e3d5913c6bbcf0
herryminh <hoanghau12357@gmail.com>
2026-06-08 19:33:53 +0700

    fix mongodb connection with env

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,17 +1,31 @@
+// const mongoose = require('mongoose');
+
+// async function connect() {
+//     try {
+//         await mongoose.connect('mongodb://localhost:27017/hoangminh_Academy_dev', {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//         });
+
+//         console.log('connect success !!!!');
+//     } catch (error) {
+//         console.error('connect fail !!!:', error);
+//     }
+   
+// }
+
+// module.exports = { connect };
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/hoangminh_Academy_dev', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGO_URI);
 
         console.log('connect success !!!!');
     } catch (error) {
         console.error('connect fail !!!:', error);
     }
-   
 }
 
 module.exports = { connect };
